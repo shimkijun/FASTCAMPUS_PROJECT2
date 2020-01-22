0c4024ceb5df9132e9cf5ce54d75b7cf752e1131
shimkijun <shimkijun@gmail.com>
2020-01-22 13:46:09 +0900

    Mockito를 활용하여 service 태스트 만들기 완료

--- src/main/java/com/example/user_project/domain/dto/Birthday.java
+++ src/main/java/com/example/user_project/domain/dto/Birthday.java
@@ -1,12 +1,9 @@
 package com.example.user_project.domain.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.time.LocalDate;
 
 @Embeddable
--- src/test/java/com/example/user_project/service/PersonServiceTest.java
+++ src/test/java/com/example/user_project/service/PersonServiceTest.java
@@ -2,10 +2,12 @@ package com.example.user_project.service;
 
 import com.example.user_project.controller.dto.PersonDto;
 import com.example.user_project.domain.Person;
+import com.example.user_project.domain.dto.Birthday;
 import com.example.user_project.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -56,10 +58,116 @@ class PersonServiceTest {
 
     @Test
     void put(){
-        PersonDto dto = PersonDto.of("martin","programming","판교", LocalDate.now(),"programmer","010-1111-1111");
-
-        personService.put(dto);
-        verify(personRepository,times(1)).save(any(Person.class));
+        personService.put(mockPersonDto());
+        verify(personRepository,times(1)).save(argThat(new IsPersonWillBeInserted()));
     }
 
+    @Test
+    void modifyIfPersonNotFound(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+        assertThrows(RuntimeException.class, () -> personService.modify(1L,mockPersonDto()));
+    }
+
+    @Test
+    void modifyIfNameDifferent() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("tony")));
+        assertThrows(RuntimeException.class, () ->personService.modify(1L,mockPersonDto()));
+    }
+
+    @Test
+    void modify(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        personService.modify(1L,mockPersonDto());
+
+        verify(personRepository,times(1)).save(argThat(new IsPersonWillBeUpdated()));
+    }
+
+    @Test
+    void modifyByNameIfPersonNotFound(){
+        when(personRepository.findById(1L))
+            .thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> personService.modify(1L,"daniel"));
+    }
+
+    @Test
+    void modifyByName(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+        personService.modify(1L,"daniel");
+
+        verify(personRepository,times(1)).save(argThat(new IsNameWillBeUpdated()));
+    }
+
+    @Test
+    void deleteIfPersonNotFound(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+        assertThrows(RuntimeException.class, () -> personService.delete(1L));
+    }
+
+    @Test
+    void delete(){
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+        personService.delete(1L);
+        verify(personRepository,times(1)).save(argThat(new IsPersonWillBeDeleted()));
+    }
+
+    private PersonDto mockPersonDto(){
+        return PersonDto.of("martin","programming","판교", LocalDate.now(),"programmer","010-1111-1111");
+    }
+
+    private  static  class IsPersonWillBeInserted implements ArgumentMatcher<Person>{
+
+        @Override
+        public boolean matches(Person person) {
+            return equals(person.getName() ,"martin")
+                    && equals(person.getHobby(),"programming")
+                    && equals(person.getAddress(),"판교")
+                    && equals(person.getBirthday(),Birthday.of(LocalDate.now()))
+                    && equals(person.getJob(),"programmer")
+                    && equals(person.getPhoneNumber(),"010-1111-1111");
+        }
+
+        private boolean equals(Object actual , Object expected){
+            return expected.equals(actual);
+        }
+    }
+
+    private  static  class IsPersonWillBeUpdated implements ArgumentMatcher<Person>{
+
+        @Override
+        public boolean matches(Person person) {
+            return equals(person.getName() ,"martin")
+                    && equals(person.getHobby(),"programming")
+                    && equals(person.getAddress(),"판교")
+                    && equals(person.getBirthday(),Birthday.of(LocalDate.now()))
+                    && equals(person.getJob(),"programmer")
+                    && equals(person.getPhoneNumber(),"010-1111-1111");
+        }
+
+        private boolean equals(Object actual , Object expected){
+            return expected.equals(actual);
+        }
+    }
+
+    private static class IsNameWillBeUpdated implements ArgumentMatcher<Person>{
+
+        @Override
+        public boolean matches(Person person) {
+            return person.getName().equals("daniel");
+        }
+    }
+
+    private static class IsPersonWillBeDeleted implements ArgumentMatcher<Person>{
+        @Override
+        public boolean matches(Person person) {
+            return person.isDeleted();
+        }
+    }
 }
